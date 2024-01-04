f7ff72bf81dd3a9f1bc084090a3a6627aacc4668
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2024-01-04 11:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const form = document.getElementById("form 1");
 function getFormvalue() {
     //Write your code here
+	const fname = form.fname.value;
+	const lname = form.lname.value;
+	alert(fname + " " + lname)
 
 }
